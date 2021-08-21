c6c381d174ffe6f19cb426efdca1ecfd4f6f2bec
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 21:06:52 -0400

    Update IngestorInterface.py

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -1,1 +1,21 @@
-"""Boilerplate model for the all documents that we will use for parsing the contents."""from abc import ABC, abstractmethodfrom typing import Listfrom .QuoteModel import QuoteModelclass IngestorInterface(ABC):        allowed_extensions = []        @classmethod    def can_ingest(cls, path:str) -> bool:        ext = path.split('.')[-1]        return ext in cls.allowed_extensions    @classmethod    @abstractmethod    def parse(cls, path:str) -> List[QuoteModel]:        pass
+""" Boilerplate model for the all documents that we will use for 
+parsing the contents.
+"""
+
+from abc import ABC, abstractmethod
+from typing import List
+from .QuoteModel import QuoteModel
+
+class IngestorInterface(ABC):
+    
+    allowed_extensions = []
+    
+    @classmethod
+    def can_ingest(cls, path:str) -> bool:
+        ext = path.split('.')[-1]
+        return ext in cls.allowed_extensions
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, path:str) -> List[QuoteModel]:
+        pass
