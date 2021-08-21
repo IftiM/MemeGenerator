66fba8c4a5a58ba3dbdedaf3e5b73d8d3719668a
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 21:05:23 -0400

    Update __init__.py

--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -1,1 +1,6 @@
-from .DocxIngestor import DocxIngestorfrom .TextIngestor import TextIngestorfrom .CSVIngestor import CSVIngestorfrom .PDFIngestor import PDFIngestorfrom .Ingestor import Ingestor
+from .DocxIngestor import DocxIngestor
+from .TextIngestor import TextIngestor
+from .CSVIngestor import CSVIngestor
+from .PDFIngestor import PDFIngestor
+from .Ingestor import Ingestor
+
