920dff02090638d3d4fd2f34347b48a8739a417a
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 21:07:08 -0400

    Update PDFIngestor.py

--- QuoteEngine/PDFIngestor.py
+++ QuoteEngine/PDFIngestor.py
@@ -1,1 +1,30 @@
-"""Parsing the contents of a pdf file."""from typing import Listfrom .IngestorInterface import IngestorInterfacefrom .QuoteModel import QuoteModelimport subprocessimport osimport randomclass PDFIngestor(IngestorInterface):    allowed_extensions = ['pdf']    @classmethod    def parse(cls, path: str) -> List[QuoteModel]:        if not cls.can_ingest(path):            raise Exception('Cannot Ingest Exception')        tmp = f'./tmp/{random.randint(0,1000000)}.txt'        call = subprocess.call(['pdftotext', path, tmp])        quotes = []        with open(tmp, "r", encoding='utf-8-sig') as t:            for index, line in enumerate(t):                parsed = line.strip().split('-')                if parsed != ['']:                    new_quote = QuoteModel(parsed[0], parsed[1])                    quotes.append(new_quote)        return quotes
+""" Parsing the contents of a pdf file.
+"""
+
+from typing import List
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+import subprocess
+import os
+import random
+
+
+class PDFIngestor(IngestorInterface):
+    allowed_extensions = ['pdf']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception('Cannot Ingest Exception')
+
+        tmp = f'./tmp/{random.randint(0,1000000)}.txt'
+        call = subprocess.call(['pdftotext', path, tmp])
+
+        quotes = []
+        with open(tmp, "r", encoding='utf-8-sig') as t:
+            for index, line in enumerate(t):
+                parsed = line.strip().split('-')
+                if parsed != ['']:
+                    new_quote = QuoteModel(parsed[0], parsed[1])
+                    quotes.append(new_quote)
+        return quotes
