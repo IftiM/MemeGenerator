62874c18853e4eae7b6a194702997019ea389aa5
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 20:47:08 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .DocxIngestor import DocxIngestorfrom .TextIngestor import TextIngestorfrom .CSVIngestor import CSVIngestorfrom .PDFIngestor import PDFIngestorfrom .Ingestor import Ingestor
