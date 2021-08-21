a08d6bc8561a3836d07f7530e719b2290dd2f230
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 20:57:04 -0400

    Initial QuoteEngine Module

---
+++ QuoteEngine/QuoteModel.py
@@ -0,0 +1,1 @@
+"""Quotemodel taking body and author - two portion of the textsderived from the documents, and returning an output."""class QuoteModel():    def __init__(self, body, author):        self.body = body        self.author = author    def __repr__(self):        return (f'{self.body} - {self.author}')
---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,1 @@
+from .DocxIngestor import DocxIngestorfrom .TextIngestor import TextIngestorfrom .CSVIngestor import CSVIngestorfrom .PDFIngestor import PDFIngestorfrom .Ingestor import Ingestor
