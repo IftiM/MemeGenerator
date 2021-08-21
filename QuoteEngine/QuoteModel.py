76231d7a7ba6945d456b48d0d6ce27e6dafe2d43
Ifti Mustafa <55010073+IftiM@users.noreply.github.com>
2021-08-20 21:07:22 -0400

    Update QuoteModel.py

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,1 +1,11 @@
-"""Quotemodel taking body and author - two portion of the textsderived from the documents, and returning an output."""class QuoteModel():    def __init__(self, body, author):        self.body = body        self.author = author    def __repr__(self):        return (f'{self.body} - {self.author}')
+""" Quotemodel taking body and author - two portion of the texts
+derived from the documents, and returning an output.
+"""
+
+class QuoteModel():
+    def __init__(self, body, author):
+        self.body = body
+        self.author = author
+
+    def __repr__(self):
+        return (f'{self.body} - {self.author}')
